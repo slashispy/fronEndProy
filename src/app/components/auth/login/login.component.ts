e9c44c96019af920b57ae27e4bbd301cf21d6c04
slashispy <andresplanas91@gmail.com>
2019-06-10 09:30:32 -0400

    Se agrego soporte al returnUrl

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 import { LoginService } from '../../../servicios/login.service';
 import { Credenciales } from '../../../clases/credenciales';
 import { AppComponent } from '../../../app.component';
+import { first } from 'rxjs/operators';
 
 
 @Component({
@@ -17,32 +18,44 @@ export class LoginComponent implements OnInit {
     usuario: new FormControl('', Validators.required),
     password: new FormControl('', Validators.required)
   });
-
+  loading = false;
+  submitted = false;
   cre: Credenciales;
   alert: boolean;
+  returnUrl: string;
 
 
   constructor(private loginService: LoginService,
               private router: Router,
+              private route: ActivatedRoute,
               private app: AppComponent) {
                 app.navBar = false;
                }
 
   ngOnInit() {
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
 
+  get f() { return this.loginForm.controls; }
+
   login() {
+    this.submitted = true;
+
     if (this.loginForm.invalid) {
       return;
     }
+    this.loading = true;
+
     const credenciales = this.loginForm.value;
     this.cre = credenciales;
     this.loginService.login(credenciales)
+    .pipe(first())
     .subscribe(resp => {
-      this.router.navigate(['/home']);
+      this.router.navigate([this.returnUrl]);
       this.app.navBar = true;
     }, errorCode => {
       this.alert = true;
+      this.loading = false;
     } );
   }
 
